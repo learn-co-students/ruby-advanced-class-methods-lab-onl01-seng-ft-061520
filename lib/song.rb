2c9ef736474c9f8737e35dd2b59dd0ca746c8ce2
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-26 23:44:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -33,11 +33,19 @@ class Song
     @@all.find{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
+=begin if find by name is false .. create
+=end  else -- find by name  .. can i do this using a boolean operator??
+
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end 
   end
   
   def self.alphabetical
-    @@all.sort 
+    @@all.sort_by{|i| i.name}  
   end
 
   def self.new_from_filename
