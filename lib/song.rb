6478c1e73035546d872672618f163e7469349c47
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-26 23:03:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,38 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song 
+    song
+  end 
+  
+  def self.new_by_name(name)
+    
+  end 
+
+  def self.create_by_name
+  end
+  
+  def self.find_by_name
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name
+  end
+  
+  def self.alphabetical
+    @@all.sort 
+  end
+
+  def self.new_from_filename
+  end 
+
+  def self.create_from_filename
+  end
+  
+  def self.destroy_all
+    @all.clear
+  end 
+
 end
